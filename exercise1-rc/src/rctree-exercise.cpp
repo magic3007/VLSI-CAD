95d30937b3d58d647b6a6ca9200e5ec006e07153
Jing Mai <jingmai@pku.edu.cn>
2021-06-26 16:32:49 +0800

    update

--- exercise1-rc/src/rctree-exercise.cpp
+++ exercise1-rc/src/rctree-exercise.cpp
@@ -7,17 +7,48 @@
 #include "rctree-simplified.hpp"
 
 void Rct::update_load(RctNode *u, RctNode *parent) {
-  // TO IMPLEMENT
+    // TO IMPLEMENT
+    u->load = 0;
+    for (auto &edge : u->fanout) {
+        auto &to = edge->to;
+        if (to == parent) continue;
+        update_load(to, u);
+        u->load += to->load;
+    }
+    u->load += u->ncap;
 }
 
 void Rct::update_delay(RctNode *u, RctNode *parent) {
-  // TO IMPLEMENT
+    // TO IMPLEMENT
+    for (auto &edge : u->fanout) {
+        auto &to  = edge->to;
+        auto &res = edge->res;
+        if (to == parent) continue;
+        to->delay = u->delay + res * to->load;
+        update_delay(to, u);
+    }
 }
 
 void Rct::update_ldelay(RctNode *u, RctNode *parent) {
-  // TO IMPLEMENT
+    // TO IMPLEMENT
+    u->ldelay = 0;
+    for (auto &edge : u->fanout) {
+        auto &to = edge->to;
+        if (to == parent) continue;
+        update_ldelay(to, u);
+        u->ldelay += to->ldelay;
+    }
+    u->ldelay += u->ncap * u->delay;
 }
 
 void Rct::update_response(RctNode *u, RctNode *parent) {
-  // TO IMPLEMENT
+    // TO IMPLEMENT
+    for (auto &edge : u->fanout) {
+        auto &to  = edge->to;
+        auto &res = edge->res;
+        if (to == parent) continue;
+        to->beta = u->beta + res * to->ldelay;
+        to->impulse = to->beta * 2. - to->delay * to->delay;
+        update_response(to, u);
+    }
 }
